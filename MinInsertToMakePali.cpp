5f469c44c7bb2b66750833fc555a1bd80a2e7a60
vivek Singh <vivekkumar52522@gmail.com>
2023-04-22 11:42:53 +0530

    DP Make Pali Wit min Insert

---
+++ MinInsertToMakePali.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+/*Input: s = "zzazz"
+Output: 0
+Explanation: The string "zzazz" is already palindrome we do not need any insertions.
+Input: s = "mbadm"
+Output: 2
+Explanation: String can be "mbdadbm" or "mdbabdm".*/
+ int minInsertions(string s) {
+       string rev=s;
+       reverse(rev.begin(),rev.end());
+       int n=s.length();
+       int dp[n+1][n+1]; 
+       for(int i=0;i<n+1;i++){
+           for(int j=0;j<n+1;j++){
+               if(i==0 || j==0){
+                   dp[i][j]=0;
+               }
+           }
+       }
+       for(int i=1;i<n+1;i++){
+           for(int j=1;j<n+1;j++){
+               if(s[i-1] ==rev[j-1]){
+                   dp[i][j]=1+dp[i-1][j-1];
+               }else{
+                   dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+               }
+           }
+       }
+       return n-dp[n][n];
+    }
